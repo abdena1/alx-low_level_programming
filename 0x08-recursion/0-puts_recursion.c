3ec06275cd97936f363777a1c7441925b77f89dc
abdellatif nadir <nadirabdellatif50@gmail.com>
2023-07-06 16:58:16 -0700

    girsttask

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
  * _puts_recursion - function like puts();
- * @s:input
- * Return: Always 0(success)
-*/
+ * @s: input
+ * Return: Always 0 (Success)
+ */
 void _puts_recursion(char *s)
 {
-	if(*s)
+	if (*s)
 	{
 		_putchar(*s);
-		_putchar_recursion(s + 1);
+		_puts_recursion(s + 1);
 	}
 
 	else
-		_putchar("\n");
+		_putchar('\n');
 }
