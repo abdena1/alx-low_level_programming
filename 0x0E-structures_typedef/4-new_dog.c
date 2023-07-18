be930e5c72780d6d651c962912559cd8753b7ff0
abdellatif nadir <nadirabdellatif50@gmail.com>
2023-07-18 03:42:27 -0700

    task num 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include "dog.h"
 
+
 /**
  * _strlen - returns the length of a string
  * @s: string to evaluate
@@ -10,9 +11,10 @@
 int _strlen(char *s)
 {
 	int i;
+
 	i = 0;
 
-while (s[i] != '\0')
+	while (s[i] != '\0')
 	{
 		i++;
 	}
@@ -20,8 +22,9 @@ while (s[i] != '\0')
 	return (i);
 }
 
+
 /**
- *_strcpy - copies the string pointed to by src
+ * *_strcpy - copies the string pointed to by src
  * including the terminating null byte (\0)
  * to the buffer pointed to by dest
  * @dest: pointer to the buffer in which we copy the string
@@ -32,14 +35,15 @@ while (s[i] != '\0')
 char *_strcpy(char *dest, char *src)
 {
 	int len, i;
+
 	len = 0;
 
-while (src[len] != '\0')
+	while (src[len] != '\0')
 	{
 		len++;
 	}
 
-for (i = 0; i < len; i++)
+	for (i = 0; i < len; i++)
 	{
 		dest[i] = src[i];
 	}
@@ -68,6 +72,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (dog == NULL)
 		return (NULL);
 
+
 	dog->name = malloc(sizeof(char) * (len1 + 1));
 	if (dog->name == NULL)
 	{
@@ -85,5 +90,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	_strcpy(dog->owner, owner);
 	dog->age = age;
 
+
 	return (dog);
 }
+
