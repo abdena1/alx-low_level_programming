f1b971b27600f055e10a1863e22e0e8041627cc5
abdellatif nadir <nadirabdellatif50@gmail.com>
2023-10-12 13:49:09 -0700

    tasks

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,39 +1,19 @@
 #include "lists.h"
 
 /**
- * add_dnodeint_end - adds a new node at the end of a dlistint_t list.
- * @head: double pointer to the head of the list
- * @n: value to store in the new node
+ * free_dlistint - Frees a dlistint_t list.
+ * @head: Pointer to the head of the list.
  *
- * Return: the address of the new element,
- *         or NULL if it failed
+ * Return: void.
  */
-dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+void free_dlistint(dlistint_t *head)
 {
-	dlistint_t *new_node, *current;
+	dlistint_t *current = head, *next;
 
-	if (!head)
-		return (NULL);
-	new_node = malloc(sizeof(dlistint_t));
-	if (!new_node)
-		return (NULL);
-
-	new_node->n = n;
-	new_node->prev = NULL;
-
-	if (!*head) /* empty list*/
+	while (current != NULL)
 	{
-		new_node->prev = NULL;
-		*head = new_node;
-		return (new_node);
+		next = current->next;
+		free(current);
+		current = next;
 	}
-	/* find the lat node */
-	current = *head;
-	while (current->next)
-		current = current->next;
-
-	current->next = new_node;
-	new_node->prev = current;
-
-	return (new_node);
 }
